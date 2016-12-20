151225ab6e3612135b3e44cd19e7445ccda93ccd
mattgrobelny <mattgrobelny@gmail.com>
2016-12-20 14:13:02 -0600

    slowly making spades run again,

--- Spades_asm.sh
+++ Spades_asm.sh
@@ -2,50 +2,64 @@
 # requires seq_crumbs
 # requires SPades
 
-files="../mid_MID1.sff
-../mid_MID2.sff
-../mid_MID3.sff
-../mid_MID4.sff
-../mid_MID5.sff
-../mid_MID6.sff"
+files="mid_MID1.sff"
+# mid_MID2.sff
+# mid_MID3.sff
+# mid_MID4.sff
+# mid_MID5.sff
+# mid_MID6.sff"
+
+
+# location of inpurt mid files
+mid_file_dir='/home/mgrobelny/Scripts/github/Thesis_code/454_raw_reads/'
+
+# location of outputs
+output_dir='/home/mgrobelny/Scripts/github/Thesis_code/454_opt_spades/'
+
+# name of stats collection file
+file_out_text='mid1_6_cleaning_data.txt'
+
+
 
 time=$(date)
-echo $time > mid1_6_cleaning_data.txt
-echo "Cleaning of:" >> mid1_6_cleaning_data.txt
-echo $files >> mid1_6_cleaning_data.txt
+echo $time > $output_dir$file_out_text
+echo "Cleaning of:" >> $output_dir$file_out_text
+echo $files >> $output_dir$file_out_text
 
 for file in $files
 do
 	echo "	"
+	working_file=$mid_file_dir$file
 	echo "working on $file"
 
 	# extract file name string
 	fastq=".fastq"
 
-	filename=$(echo $file | grep -oE "mid_MID[0-9]")
+	filename=${file::-4}
 
 
 	# extract sff to fastq
 	echo "extracting sff"
-	echo "	" >> mid1_6_cleaning_data.txt
-	sff_extract $file > $filename$fastq
+	echo "	" >> $output_dir$file_out_text
+	sff_extract $working_file > $output_dir$filename$fastq
 
 
 	# trim adapters
 	echo "Trimming adapters..."
 	adapter_trim="_AT"
-	filename_out1=$filename$adapter_trim
-	adapter_array=($(tagcleaner -predict -fastq mid_MID1.fastq | sed -E "1d; s/tag.\t([ATGCN]+)\t.+\t.+/\1\n/"))
-	tagcleaner -tag3 ${adapter_array[0]} -tag5 ${adapter_array[1]} -out_format 3 -out $filename_out1 -fastq $filename$fastq
+	filename_out1=$output_dir$filename$adapter_trim
+	adapter_array=($(tagcleaner -predict -fastq $output_dir$filename$fastq | sed -E "1d; s/tag.\t([ATGCN]+)\t.+\t.+/\1\n/"))
+	tagcleaner -tag3 ${adapter_array[0]} -tag5 ${adapter_array[1]} -out_format 3 -out $filename_out1 -fastq $output_dir$filename$fastq
 
 	num_nuc_end2=$(sed -n 2~4p $filename$adapter_trim$fastq | tr -d '\n' | wc -m)
 	num_nuc_start2=$(sed -n 2~4p $filename$fastq | tr -d '\n' | wc -m)
 	delta2=$(bc <<< "scale = 2; (1-($num_nuc_end2/$num_nuc_start2))*100")
-	echo "Tags used for $file:" >> mid1_6_cleaning_data.txt
-	echo "-tag3 ${adapter_array[0]}" >> mid1_6_cleaning_data.txt
-	echo "-tag5 ${adapter_array[1]}" >> mid1_6_cleaning_data.txt
-	echo "Percent of nucleotides trimmed as tags/adapters $delta2 %" >> mid1_6_cleaning_data.txt
-	echo "	" >> mid1_6_cleaning_data.txt
+	echo "Tags used for $file:" >> $output_dir$file_out_text
+	echo "-tag3 ${adapter_array[0]}" >> $output_dir$file_out_text
+	echo "-tag5 ${adapter_array[1]}" >> $output_dir$file_out_text
+
+	echo "Percent of nucleotides trimmed as tags/adapters $delta2 %" >> $output_dir$file_out_text
+	echo "	" >> $output_dir$file_out_text
 
 	# trim edges
 	left_clip=30
@@ -54,49 +68,49 @@ do
 	echo "Clipping edges..."
 	echo "Left clip: $left_clip"
 	echo "Right clip: $right_clip"
-	filename_out2=$filename$adapter_trim$trim_edge$fastq
+	filename_out2=$output_dir$filename$adapter_trim$trim_edge$fastq
 	trim_edges -l $left_clip -r $right_clip -o $filename_out2 $filename_out1$fastq
 
 	num_nuc_start3=$(sed -n 2~4p $filename_out1$fastq | tr -d '\n' | wc -m)
 	num_nuc_end3=$(sed -n 2~4p $filename_out2 | tr -d '\n' | wc -m)
 	delta3=$(bc <<< "scale = 2; (1-($num_nuc_end3/$num_nuc_start3))*100")
-	echo "Percent of nucleotides trimmed using edge clipping: ~ $delta3 %" >> mid1_6_cleaning_data.txt
-	echo "	" >> mid1_6_cleaning_data.txt
+	echo "Percent of nucleotides trimmed using edge clipping: ~ $delta3 %" >> $output_dir$file_out_text
+	echo "	" >> $output_dir$file_out_text
 
 	# filter by blast
 	echo "filtering by blast "
 	vector_trim="_VF"
 	filtered_out_file="_filtered_out_seqs.fastq"
 	filter_id=98
-	echo "Filtering with $filter_id % id" >> mid1_6_cleaning_data.txt
-	filter_by_blast -b vectors_454.fasta -e $filename$filtered_out_file -s $filter_id -o $filename$adapter_trim$trim_edge$vector_trim$fastq $filename_out2
+	echo "Filtering with $filter_id % id" >> $output_dir$file_out_text
+	filter_by_blast -b vectors_454.fasta -e $output_dir$filename$filtered_out_file -s $filter_id -o $output_dir$filename$adapter_trim$trim_edge$vector_trim$fastq $filename_out2
 
 	# Write how much was filtered
-	echo "Filtering by blast stats for: $filename.sff" >> mid1_6_cleaning_data.txt
+	echo "Filtering by blast stats for: $filename.sff" >> $output_dir$file_out_text
 	start_num=$(grep -c "^@I" $filename_out2)
-	echo "Starting num of reads: $start_num" >> mid1_6_cleaning_data.txt
+	echo "Starting num of reads: $start_num" >> $output_dir$file_out_text
 	after_filter=$(grep -c "^@I" $filename$adapter_trim$trim_edge$vector_trim$fastq)
-	echo "After filtering: $after_filter" >> mid1_6_cleaning_data.txt
+	echo "After filtering: $after_filter" >> $output_dir$file_out_text
 	delta=$(bc <<< "scale = 2; $start_num-$after_filter")
-	echo "Num of reads filtered out: $delta" >> mid1_6_cleaning_data.txt
-	echo "	" >> mid1_6_cleaning_data.txt
+	echo "Num of reads filtered out: $delta" >> $output_dir$file_out_text
+	echo "	" >> $output_dir$file_out_text
 
 	# trim by quality
 	echo "Trimming by quality..."
 	quality_thresh=15
 	quality_clip="_QC"
-	echo "Quality threshold: $quality_thresh" >> mid1_6_cleaning_data.txt
-	filename_out3=$filename$adapter_trim$trim_edge$vector_trim$quality_clip$fastq
-	trim_quality -q $quality_thresh -o $filename_out3 $filename$adapter_trim$trim_edge$vector_trim$fastq
+	echo "Quality threshold: $quality_thresh" >> $output_dir$file_out_text
+	filename_out3=$output_dir$filename$adapter_trim$trim_edge$vector_trim$quality_clip$fastq
+	trim_quality -q $quality_thresh -o $filename_out3 $output_dir$filename$adapter_trim$trim_edge$vector_trim$fastq
 
-	num_nuc_start4=$(sed -n 2~4p $filename$adapter_trim$trim_edge$vector_trim$fastq | tr -d '\n' | wc -m)
+	num_nuc_start4=$(sed -n 2~4p $output_dir$filename$adapter_trim$trim_edge$vector_trim$fastq | tr -d '\n' | wc -m)
 	num_nuc_end4=$(sed -n 2~4p $filename_out3 | tr -d '\n' | wc -m)
 	delta4=$(bc <<< "scale = 2; (1-($num_nuc_end4/$num_nuc_start4))*100")
-	echo "Percent of nucleotides trimmed using quality clipping: $delta4 %" >> mid1_6_cleaning_data.txt
+	echo "Percent of nucleotides trimmed using quality clipping: $delta4 %" >> $output_dir$file_out_text
 
 	# assembly with Spades
 	echo "assembling with Spades"
-	out_file="~/Data/mids_all/cleaned_mids/Spades_output_trimmed_careful_"
+	out_file="/home/mgrobelny/Scripts/github/Thesis_code/454_opt_spades/Spades_output_trimmed_careful_"
 	if (("$filename" == "mid_MID6"));
 	then
 	spades.py --trusted-contigs ~/Desktop/Spades_run/trust.fasta -m 10 -t 16 --careful --s1 $filename_out3 -o $out_file$filename
@@ -113,17 +127,17 @@ do
 	largest_contig=$( cat $contigs_file |grep ">" | sed -E 's/>NODE_([0-9]+)_length_([0-9]+)_cov_([0-9]+)/\1\t\2\t\3/'| cut -f 2 | head -n 1)
 
 	#write contig stats to file
-	echo "	" >> mid1_6_cleaning_data.txt
-	echo "Contig stats for $filename_out3 assembly" >> mid1_6_cleaning_data.txt
-	echo "Total contigs: $num_contigs" >> mid1_6_cleaning_data.txt
-	echo "Largest contig: $largest_contig" >> mid1_6_cleaning_data.txt
-	echo "	" >> mid1_6_cleaning_data.txt
-	echo "#-------------------------------------------------------------------------------#" >> mid1_6_cleaning_data.txt
+	echo "	" >> $output_dir$file_out_text
+	echo "Contig stats for $filename_out3 assembly" >> $output_dir$file_out_text
+	echo "Total contigs: $num_contigs" >> $output_dir$file_out_text
+	echo "Largest contig: $largest_contig" >> $output_dir$file_out_text
+	echo "	" >> $output_dir$file_out_text
+	echo "#-------------------------------------------------------------------------------#" >> $output_dir$file_out_text
 
 	assembly_data="_Asm_Data.tsv"
-	echo "Contig_num	Contig_len	Contig_cov" > $filename$adapter_trim$trim_edge$vector_trim$quality_clip$assembly_data
-	echo "$contig_stats" >> $filename$adapter_trim$trim_edge$vector_trim$quality_clip$assembly_data
-	echo "done with $file.sff"
+	echo "Contig_num	Contig_len	Contig_cov" > $output_dir$filename$adapter_trim$trim_edge$vector_trim$quality_clip$assembly_data
+	echo "$contig_stats" >> $output_dir$filename$adapter_trim$trim_edge$vector_trim$quality_clip$assembly_data
+	echo "done with $file"
 done
 
 
--- thesis_code.sh
+++ thesis_code.sh
@@ -4,8 +4,10 @@
 # Matt Grobelny
 
 # If true run 454 newbler assembly optimization
-calculate_opi_454_asm=1
-one=1
+calculate_opi_454_asm=0
+
+calculate_opi_spades_asm=1
+
 ################################################################################
 if (($calculate_opi_454_asm == "1"))
 	then
@@ -25,10 +27,11 @@ if (($calculate_opi_454_asm == "1"))
 	wk_output=${file::-4}$output
 	wk_file=$mid_file_dir$file
 	echo "perl runAssembly_PS.pl 15 45 5 15 50 5 95 99 1 $wk_output ../bothtrimfiles.fasta ../$wk_file TRUE TRUE"
-
+go
 	done
-# # elif
-# # then
+elif (($calculate_opi_spades_asm == "1"))
+	then
+		/home/mgrobelny/Scripts/github/Thesis_code/Spades_asm.sh
 else
 		echo "done"
 fi
